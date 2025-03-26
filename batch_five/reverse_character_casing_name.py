c2521ae86cdda67b3c4881cc228d2b0ae4ae48f0
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 13:33:08 +0800

    ask_for_the_user_name_input

--- batch_five/reverse_character_casing_name.py
+++ batch_five/reverse_character_casing_name.py
@@ -1,3 +1,5 @@
 # ask the user to input their full name in incorrect casing
+full_name = input("What is your full name? ")
+
 # convert each character if the input in reverse casing
 # print the full name in reverse casing
