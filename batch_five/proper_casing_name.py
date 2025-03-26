0479a2216f21675fa5e1d7ffc1ac9ac41d733f60
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 12:45:21 +0800

    ask_for_name_input_and_convert_to_proper_casing

--- batch_five/proper_casing_name.py
+++ batch_five/proper_casing_name.py
@@ -1,3 +1,6 @@
 # ask for the user's full name
+full_name = input("What is your full name? ")
+
 # convert the full name to proper casing, capitalizing the first letter of each word
 # print the full name in proper casing
+print(full_name.title())
