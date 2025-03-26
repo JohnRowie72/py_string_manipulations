d45b48b8c71f46c532e8a299c808e5c2c01b5fc4
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 15:00:00 +0800

    ask_user_for_name_input

--- batch_five/name_snake_case.py
+++ batch_five/name_snake_case.py
@@ -1,3 +1,5 @@
 # ask the user to input their full name in incorrect casing
+full_name = input("What is your full name? ")
+
 # convert the full name to snake casing, replacing spaces with underscores
 # print the full name in snake casing
