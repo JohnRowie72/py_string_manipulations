c4af6deff785882c5cc57d4eec6dec4aa58b87cc
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 14:38:54 +0800

    ask_for_the_user_name_input

--- batch_five/number_of_characteristics_in_the_input.py
+++ batch_five/number_of_characteristics_in_the_input.py
@@ -1,3 +1,5 @@
 # ask the user's input for their full name
+full_name = input("What is your full name? ")
+
 # count the number of characters in the full name, including spaces
 # print the number of characters in the full name
