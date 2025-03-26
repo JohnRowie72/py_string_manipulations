c152bf14cc8c259edc787fdf418ecb4236542fa0
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 12:32:14 +0800

    ask_user_for_number_input

--- batch_five/format_number_to_six_digits_with_leading_zeros.py
+++ batch_five/format_number_to_six_digits_with_leading_zeros.py
@@ -1,3 +1,5 @@
 # ask user to input a number ranging from 0 to 1000
+number = int(input("Enter a number between 0 and 1000: "))
+
 # convert the number to a 6-digit string with leading zeros
 # print the formatted number
