95ea2f4f005697b2feeffb588dc0ff406b21fede
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 12:19:17 +0800

    ask_for_input_and_display_formatted_input

--- batch_five/remove_leading_spaces.py
+++ batch_five/remove_leading_spaces.py
@@ -1,3 +1,6 @@
 # ask for the user's full name
+full_name = input("What is your full name? ")
+
 # remove any leading spaces from the full name
 # print the full name without leading spaces
+print(full_name.lstrip())
