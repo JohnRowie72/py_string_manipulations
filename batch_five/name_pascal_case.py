8e4f57ea410eedc52e2818b09be9abaf8001ef42
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 14:47:44 +0800

    ask_user_for_name_input

--- batch_five/name_pascal_case.py
+++ batch_five/name_pascal_case.py
@@ -1,3 +1,5 @@
 # ask the user to input their full name in incorrect casing
+full_name = input("What is your full name? ")
+
 # convert the full name to pascal casing, capitalizing the first letter of each word
 # print the full name in pascal casing
