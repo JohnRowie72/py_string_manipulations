0167fd221fdc0604f9cbc02feb5ba4d5814fc157
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 14:16:59 +0800

    count_and_print_the_number_of_words_in_the_sentence

--- batch_five/number_of_words_in_the_input.py
+++ batch_five/number_of_words_in_the_input.py
@@ -4,3 +4,4 @@ statement = input("Enter a complete sentence: ")
 # split the sentence into words 
 # count the number of words in the sentence
 # print the number of words in the sentence
+print(len(statement.split()))
