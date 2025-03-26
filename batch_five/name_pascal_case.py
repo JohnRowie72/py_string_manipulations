86fa49cc9e1307b7c901958ac7ef0367e02c6ef0
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 14:47:20 +0800

    pseudocode

---
+++ batch_five/name_pascal_case.py
@@ -0,0 +1,3 @@
+# ask the user to input their full name in incorrect casing
+# convert the full name to pascal casing, capitalizing the first letter of each word
+# print the full name in pascal casing
