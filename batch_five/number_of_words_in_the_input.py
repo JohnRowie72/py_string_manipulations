3e9ef6bdd9b56c370a5c4c623fa48fd7d8fe7c4d
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-03-26 14:15:48 +0800

    ask_the_user_for_sentence_input

--- batch_five/number_of_words_in_the_input.py
+++ batch_five/number_of_words_in_the_input.py
@@ -1,4 +1,6 @@
 # ask user to input a complete sentence
+statement = input("Enter a complete sentence: ")
+
 # split the sentence into words 
 # count the number of words in the sentence
 # print the number of words in the sentence
